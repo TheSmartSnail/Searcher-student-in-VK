062da690da30d7ba1c1123dbed8557fa5b719274
TheSmartSnail <snail.russian@gmail.com>
2021-08-19 18:45:10 +0300

    Добавил BaseCommand

---
+++ Searcher-student-in-VK/Infrastructure/Command/BaseCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Searcher_student_in_VK.Infrastructure.Command
+{
+    internal abstract class BaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+}
